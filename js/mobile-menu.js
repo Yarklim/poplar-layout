47ec8664852d18fcc64101e8714f8ab0473e022b
Yaroslav Klimas <yarklim@gmail.com>
2023-07-18 21:08:51 +0300

    added burger & footer

---
+++ js/mobile-menu.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('.header__nav-list');
+const navBtn = document.querySelector('.header__button');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-open');
+  navMenu.classList.toggle('is-open');
+  navBtn.classList.toggle('is-open');
+});
+
+document.querySelector('.active').addEventListener('click', (e) => {
+  e.preventDefault();
+  hamburger.classList.remove('is-open');
+  navMenu.classList.remove('is-open');
+  navBtn.classList.remove('is-open');
+});
