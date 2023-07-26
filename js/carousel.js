f4ad806fa1590fd188a26a093662a2d5027dd834
Yaroslav Klimas <yarklim@gmail.com>
2023-07-26 13:06:15 +0300

    added carousel

---
+++ js/carousel.js
@@ -0,0 +1,33 @@
+const carousel = document.querySelector('.carousel__wrapper');
+
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft;
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+
+  e.preventDefault();
+  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+};
+
+const dragStop = () => {
+  isDragStart = false;
+};
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('touchstart', dragStart);
+
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('touchmove', dragging);
+
+carousel.addEventListener('mouseup', dragStop);
+carousel.addEventListener('mouseleave', dragStop);
+carousel.addEventListener('touchend', dragStop);
